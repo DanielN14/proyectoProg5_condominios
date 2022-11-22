cabfbd679ebda60f1117105167ef771649d60a21
Daniel N <jdnp2001@gmail.com>
2022-11-21 21:58:08 -0600

    Permitir nulos modelo error

--- proyectoDB2_condominios/Models/Error.cs
+++ proyectoDB2_condominios/Models/Error.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace proyectoDB2_condominios.Models
+﻿namespace proyectoDB2_condominios.Models
 {
     public class Error
     {
-        public string Message { get; set; }
+        public string? Message { get; set; }
 
-        public string BackUrl { get; set; }
+        public string? BackUrl { get; set; }
 
-        public string Text { get; set; }
+        public string? Text { get; set; }
     }
 }
